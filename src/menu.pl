5e4715d7de9f090e6ebff74b024118052adf7590
Cajo Albuquerque <up201706735@fe.up.pt>
2019-11-05 16:22:58 +0000

    Menu display and options

---
+++ src/menu.pl
@@ -0,0 +1,38 @@
+main_menu_header :-
+    write('============================================='), nl,
+    write(' _______  _______           _______  _       '), nl,
+    write('(  ____ \\(  ____ \\|\\     /|(  ____ \\| \\    /\\'), nl,
+    write('| (    \\/| (    \\/| )   ( || (    \\/|  \\  / /'), nl,
+    write('| (__    | |      | (___) || (__    |  (_/ / '), nl,
+    write('|  __)   | |      |  ___  ||  __)   |   _ (  '), nl,
+    write('| (      | |      | (   ) || (      |  ( \\ \\ '), nl,
+    write('| (____/\\| (____/\\| )   ( || (____/\\|  /  \\ \\'), nl,
+    write('(_______/(_______/|/     \\|(_______/|_/    \\/'),nl,
+    write('                                             '), nl,
+    write('============================================='), nl.
+
+main_menu_options :-
+    write(' Welcome to Echek! Please choose an option: '), nl,
+    write('      1 - Player vs Player'), nl,
+    write('      2 - Player vs Machine'), nl,
+    write('      3 - Machine vs Machine'), nl,
+    write('      4 - Exit'), nl.
+
+execute_menu_input(Input) :- !.
+    %!, Input =:= 1, start_game.
+execute_menu_input(Input) :- !.
+    %!, Input =:= 2, start_game.
+execute_menu_input(Input) :- !.
+    %!, Input =:= 3, start_game.
+execute_menu_input(Input) :-
+    !, Input =:= 4.
+execute_menu_input(_) :-
+    write('Invalid Input!').
+
+
+menu_cicle:-
+    repeat,
+        main_menu_header,
+        main_menu_options,
+        read(Input),
+        execute_menu_input(Input),
