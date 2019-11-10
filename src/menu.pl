6cbba0c0cb6813b3798d4f8ef60bf090dfc42a54
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-11-10 18:00:22 +0000

    Menu refactoring

--- src/menu.pl
+++ src/menu.pl
@@ -20,20 +20,20 @@ main_menu_options :-
     write('      3 - Machine vs Machine'), nl,
     write('      4 - Exit'), nl.
 
-execute_menu_input(Input) :-
-    !, Input =:= 1, start_game.
-execute_menu_input(Input) :- !.
+execute_menu_input(1) :-
+    !, start_game.
+execute_menu_input(2) :- !.
     %!, Input =:= 2, start_game.
-execute_menu_input(Input) :- !.
+execute_menu_input(3) :- !.
     %!, Input =:= 3, start_game.
-execute_menu_input(Input) :-
-    !, Input =:= 4.
+execute_menu_input(4) :- !.
 execute_menu_input(_) :-
-    write('Invalid Input!').
+    write('Invalid Input!'), false.
 
 echek:-
     repeat,
         main_menu_header,
         main_menu_options,
         read(Input),
-        execute_menu_input(Input).
+        execute_menu_input(Input),
+        !, Input =:= 4.
