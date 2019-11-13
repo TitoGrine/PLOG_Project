f079f1f08a0407f06c5250d87c27910fec7dc437
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-11-13 17:07:14 +0000

    Comments on utils

--- src/utils.pl
+++ src/utils.pl
@@ -1,31 +1,41 @@
 :- use_module(library(lists)).
 
+%Checks if a piece is within the 5x5 board limits
 within_limits(R, C) :-
     (R >= 0, R =< 5),
     (C >= 0, C =< 5).
 
+%Checks if a piece is within the 4x4 virtual limits (only if they exist)
 within_virtual_limits(R, C) :-
     \+ breaks_virtual_horizontal_limit(C),
     \+ breaks_virtual_vertical_limit(R).
 
+%Checks if a piece breaks the horizontal limit
 breaks_virtual_horizontal_limit(C) :-
-    (C =:= 0 ; C =:= 5),
+    (C =:= 0 ; C =:= 5),        %A piece can only break the limit if it is left or right of the 4x4 central board
     virtual_horizontal_limit.
 
+%Checks if a piece breaks the vertical limit
 breaks_virtual_vertical_limit(R) :-
-    (R =:= 0 ; R =:= 5),
+    (R =:= 0 ; R =:= 5),        %A piece can only break the limit if it is above or below the 4x4 central board
     virtual_vertical_limit.
 
+%Checks if there is a horizontal limit to the dynamic game board
 virtual_horizontal_limit :-
     cell(1, _, _, _),
     cell(4, _, _, _).
 
+%Checks if there is a vertical limit to the dynamic game board
 virtual_vertical_limit :-
     cell(_, 1, _, _),
     cell(_, 4, _, _).
 
+%Checks if the coordinates are to cancel an action
 cancel(-1, -1).
 
+%======================================
+%This predicates read coordinates from the standard input
+%They only differ in the message displayed
 read_coords_no_cancel(X, Y, Player, Piece, move) :-
     ansi_format([fg(green)],'Move ~s ~s to cell', [Player, Piece]),
     read(Input),
@@ -49,7 +59,9 @@ read_player_piece(X, Y) :-
     read(Input),
     arg(1, Input, X), arg(2, Input, Y),
     integer(X), integer(Y).
+%=====================================
 
+%Reads the name of a piece from the standard input
 choose_piece(Player, Piece) :-
     repeat,
     ansi_format([fg(green)],'Player ~s choose a piece: ', [Player]),
