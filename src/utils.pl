5b60308560f6d5d6f61c0b25382f3118102822b8
Tito Griné <tito999@gmail.com>
2019-11-16 15:33:41 +0000

    Fixed bug with infinite loop when Machine vs Machine.

--- src/utils.pl
+++ src/utils.pl
@@ -100,7 +100,20 @@ make_best_moves_list(ValuedMoves, BestValue, BestMoves) :-
 % Prevents the game entering an infinite cycle, when playing machine vs machine, and there is only one optimal move
 prevent_cycles(ValuedMoves, BestValue, OptimalMoves, BestMoves) :-
     length(OptimalMoves, Length), length(ValuedMoves, TotalLength),!,
-    (((Length > 1 ; BestValue >= 100 ; TotalLength =:= Length),
+    write('Length of optimal moves = '), write(Length), nl,
+    write('Length of all moves = '), write(TotalLength), nl,
+    ((((Length > 1 , \+ only_king_moves(OptimalMoves, Length)) ; BestValue >= 100 ; TotalLength =:= Length),
       append(OptimalMoves, [], BestMoves));
      ((repeat, random_member([RandomValue | Rest], ValuedMoves), RandomValue < BestValue),!, % Ensures the random move isn't an optimal move
+      write('Had to choose a random move.'), nl,
       append(OptimalMoves, [Rest], BestMoves))).
+
+% Checks if all the optimal moves are moving the king. When this happends it's likely that both AI's will just move their kings
+% around the board in an infinite loop. This predicate helps prevent that.
+only_king_moves(OptimalMoves, Length) :-
+    findall(Piece, (member([Piece, _, _], OptimalMoves), Piece == king), KingMoves),!,
+    length(KingMoves, KingLength),!,
+    write(OptimalMoves), nl,
+    write(KingMoves), nl,
+    write(KingLength), nl,
+    KingLength == Length.
